7d444d7f88f00ebb37e73c55b1877f4fc66fa810
jayasekher <noreply@dummy.noreply.com>
2021-06-15 16:58:07 +0530

    backlog

--- commands/run-playbook.sh
+++ commands/run-playbook.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 ansible-playbook my-nginx-install-playbook.yml --extra-vars "ansible_sudo_pass=***"
+
+ansible-playbook my-nginx-install-playbook.yml --extra-vars "ansible_sudo_pass=centos"
+
+ansible-playbook nginx-playbook.yml --extra-vars "ansible_sudo_pass=centos"
--- commands/see-var-ansible-facts.sh
+++ commands/see-var-ansible-facts.sh
@@ -3,3 +3,5 @@
 ansible centos0 -m setup --extra-vars "ansible_sudo_pass=***"
 
 ansible-playbook my-vars-playbook.yml --extra-vars "ansible_sudo_pass=***"
+
+ansible-playbook my-facts-centos0-playbook.yml --extra-vars "ansible_sudo_pass=centos"
